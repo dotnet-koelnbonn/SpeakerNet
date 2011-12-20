4127beebe3b58d39a41944e53f61ebd9ad22d425
Albert Weinert <info@der-albert.com>
2011-12-20 20:41:02 +0100

    Made From and Cc on Mails Configurable

--- SpeakerNet/Services/SendMailService.cs
+++ SpeakerNet/Services/SendMailService.cs
@@ -1,15 +1,23 @@
 using System;
 using System.Configuration;
 using System.Net.Mail;
+using SpeakerNet.Settings;
 
 namespace SpeakerNet.Services
 {
     public class SendMailService : ISendMailService
     {
+        readonly IMailSettings settings;
+
+        public SendMailService(IMailSettings settings)
+        {
+            this.settings = settings;
+        }
+
         public void SendMail(string to, string subject, string body)
         {
-            var message = new MailMessage("orga@dotnet-cologne.de", to, subject, body);
-            message.To.Add("orga@dotnet-cologne.de");
+            var message = new MailMessage(settings.From, to, subject, body);
+            message.CC.Add(settings.Cc);
             var smtp = new SmtpClient();
             smtp.Send(message);
         }
---
+++ SpeakerNet/Settings/IMailSettings.cs
@@ -0,0 +1,28 @@
+﻿namespace SpeakerNet.Settings
+{
+    public interface IMailSettings
+    {
+        string From { get; }
+        string Cc { get; }
+    }
+
+    public class MailSettings : IMailSettings
+    {
+        private readonly IAppSettings settings;
+
+        public MailSettings(IAppSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        public string From
+        {
+            get { return settings.Get<string>("Mail.From"); }
+        }
+
+        public string Cc
+        {
+            get { return settings.Get<string>("Mail.Cc"); }
+        }
+    }
+}
